477941f532597ac7678730ae8b7be51ee3ad581f
jackaharrisii <jackaharrisii@gmail.com>
2023-09-01 19:17:29 -0400

    tweaked the tests a bit for efficiency

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kankaRulerCalculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('kankaRulerCalculator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const compiled = fixture.debugElement.nativeElement;
-    fixture.detectChanges();
-    let sut = compiled.querySelector('#titleDiv').textContent;
-    expect(sut).toContain('kankaRulerCalculator');
-  });
-
 });
--- src/app/ruler-calculator/ruler-calculator.component.spec.ts
+++ src/app/ruler-calculator/ruler-calculator.component.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RulerCalculatorComponent } from './ruler-calculator.component';
 import {FormsModule} from "@angular/forms";
+import {AppComponent} from "../app.component";
 
 describe('RulerCalculatorComponent', () => {
   let component: RulerCalculatorComponent;
   let fixture: ComponentFixture<RulerCalculatorComponent>;
+  let sut: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,11 +16,21 @@ describe('RulerCalculatorComponent', () => {
     });
     fixture = TestBed.createComponent(RulerCalculatorComponent);
     component = fixture.componentInstance;
+    sut = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have as title 'Kanka Ruler Calculation'`, () => {
+    expect(component.title).toEqual('Kanka Ruler Calculator');
+  });
+
+  it('should render title', () => {
+    let actual = sut.querySelector('#title').textContent;
+    expect(actual).toContain('Kanka Ruler Calculator');
   });
 
   describe('should calculate the ', () => {
